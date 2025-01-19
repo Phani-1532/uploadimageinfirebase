1458c854d4e1076c001721c5728d10f149453e25
Phanindra <phanindragadireddy2@gmail.com>
2025-01-19 10:36:09 +0530

    storage functionality completed

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,16 +1,16 @@
-import firebase from 'firebase'
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyC6DG4QlNN-N8fVDC6oZt-jMFhL-da7-UA",
     authDomain: "upload-images-bd4c6.firebaseapp.com",
     projectId: "upload-images-bd4c6",
-    storageBucket: "upload-images-bd4c6.firebasestorage.app",
+    storageBucket: "upload-images-bd4c6.appspot.com",
     messagingSenderId: "77181420055",
     appId: "1:77181420055:web:598684c31cc3cf02499252"
-  };
+};
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
 
-const storage = firebase.storage()
-
-export default storage
+export default storage;
